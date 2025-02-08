ccdbee1e26fc2af15e2c356a1715312a11c758cf
majicmaj <majd.tarbin@outlook.com>
2025-02-08 13:29:37 -0500

    Add display for most recent guess in GuessList component

--- src/components/GuessList.tsx
+++ src/components/GuessList.tsx
@@ -18,8 +18,10 @@ export function GuessList({ guesses }: GuessListProps) {
     sort === "similarity" ? sortBySimilarity : undefined
   );
 
+  const mostRecentGuess = guesses[guesses.length - 1];
+
   return (
-    <div className="backdrop-blur-[4px] p-2 border border-zinc-200 dark:border-zinc-800 lg:p-2 pb-0 rounded-[20px] w-full h-[calc(100vh-160px)] flex flex-col items-center gap-2 overflow-auto">
+    <div className="bg-white dark:bg-black p-2 border border-zinc-200 dark:border-zinc-800 lg:p-2 pb-0 rounded-[20px] w-full h-[calc(100vh-160px)] flex flex-col items-center gap-2 overflow-auto">
       <div className="grid grid-cols-2 gap-1 w-full bg-white dark:bg-black text-black dark:text-white rounded-full p-1">
         <button
           className={`px-2 py-1 transition-all rounded-full ${
@@ -42,11 +44,56 @@ export function GuessList({ guesses }: GuessListProps) {
           Similarity
         </button>
       </div>
-      <div className="p-2 rounded-xl w-full h-[calc(100vh-160px)] flex flex-col items-center gap-2 overflow-auto">
+
+      {mostRecentGuess && (
+        <div className="max-w-md w-full bg-black text-white border px-4 py-4 rounded-2xl dark:bg-white dark:text-black">
+          <div className="flex justify-between gap-2 items-center">
+            <span className="flex-1 font-medium truncate">
+              {mostRecentGuess.guess}
+            </span>
+            <div className="flex justify-between gap-1 items-center flex-col lg:flex-row lg:gap-2">
+              <div className="flex justify-between gap-2 items-center">
+                <span className="w-10 font-normal text-sm truncate opacity-60">
+                  #{guesses?.indexOf(mostRecentGuess) + 1}
+                </span>
+
+                {mostRecentGuess.similarity !== null && (
+                  <span className="w-12 text-sm opacity-80">
+                    {(mostRecentGuess.similarity * 100).toFixed(2)}%
+                  </span>
+                )}
+              </div>
+              {mostRecentGuess.similarity !== null ? (
+                <div className="w-40 overflow-auto bg-zinc-800 dark:bg-zinc-200 rounded-full h-3">
+                  <div
+                    className="bg-[#0AC8B9] h-full rounded-full transition-all"
+                    style={{
+                      width: `${
+                        Math.max(0, mostRecentGuess.similarity) * 100
+                      }%`,
+                      backgroundSize: "100% 100%",
+                    }}
+                  ></div>
+                </div>
+              ) : (
+                <p className="text-sm text-zinc-500 truncate">
+                  {mostRecentGuess.error}
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+
+      {mostRecentGuess && (
+        <div className="border-b border-zinc-300 dark:border-zinc-700 w-[calc(100%-64px)] my-2" />
+      )}
+
+      <div className="rounded-xl w-full h-[calc(100vh-160px)] flex flex-col items-center gap-2 overflow-auto">
         {sortedGuesses.map((guess, index) => (
           <div
             key={`${guess.guess}-${index}`}
-            className="max-w-md w-full text-black px-4 py-4 rounded-2xl bg-white dark:bg-zinc-800 dark:text-white"
+            className="max-w-md w-full border border-zinc-200 dark:border-zinc-800 text-black px-4 py-4 rounded-2xl bg-white dark:bg-zinc-800 dark:text-white"
           >
             <div className="flex justify-between gap-2 items-center">
               <span className="flex-1 font-medium truncate">{guess.guess}</span>
